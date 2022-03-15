662033753e42272e86d2ece957dd36ceb5054170
chengping <chengping@expservice.com.cn>
2022-03-16 01:00:21 +0800

    项目初始化

---
+++ src/test/java/org/cp/mdq/MdqApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cp.mdq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MdqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
